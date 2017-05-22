7b902dbe7ec8d51a54e222321158f7cff00dda74
Uncle88 <andreyproskura88@gmail.com>
2017-05-22 15:41:56 +0300

    created ListDelegateFlowLayout

---
+++ ListIOSXamarinMVVMCross/ListIOSXamarinMVVMCrossIos/ListDelegateFlowLayout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ListIOSXamarinMVVMCrossIos
+{
+    public class ThingDelegateFlowLayout : UICollectionViewDelegateFlowLayout
+    {
+        List<Thing> Things;
+
+        public ThingDelegateFlowLayout(List<Thing> things)
+        {
+            Things = things;
+        }
+
+        // this is the override that allows you to dynamically size UICollectionView items
+        public override SizeF GetSizeForItem(UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+        {
+            return new SizeF(
+                width: UIScreen.MainScreen.Bounds.Width, // Set the width to whatever you want. In this case, it's the screen width.
+                height: Things[indexPath.Row].Height // Set the item height
+            );
+        }
+    }
+}
